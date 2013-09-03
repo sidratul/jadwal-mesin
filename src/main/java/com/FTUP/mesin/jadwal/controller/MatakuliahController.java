5fa57b052c0dd349771b4770349f854a905f80cd
sidratul <muhammad.mochi@gmail.com>
2013-09-03 15:04:15 +0700

    rename to jadwal-servelet

---
+++ src/main/java/com/FTUP/mesin/jadwal/controller/MatakuliahController.java
@@ -0,0 +1,15 @@
+package com.FTUP.mesin.jadwal.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("matakuliah")
+public class MatakuliahController {
+    
+    
+    @RequestMapping("edit")
+    public void editMatkul(){
+        
+    }
+}
