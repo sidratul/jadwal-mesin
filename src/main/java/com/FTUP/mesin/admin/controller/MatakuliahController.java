03f3fdb960104ab76395491795646b3b9b3909eb
sidratul <muhammad.mochi@gmail.com>
2013-09-04 09:24:39 +0700

    coba servlet. pom update(disable)

--- src/main/java/com/FTUP/mesin/admin/controller/MatakuliahController.java
+++ src/main/java/com/FTUP/mesin/admin/controller/MatakuliahController.java
@@ -1,15 +1,13 @@
-package com.FTUP.mesin.jadwal.controller;
+package com.FTUP.mesin.admin.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("matakuliah")
-public class MatakuliahController {
+public class MatakuliahController {    
     
-    
-    @RequestMapping("edit")
+    @RequestMapping("/edit")
     public void editMatkul(){
-        
-    }
+        System.out.println("masuk");
+    }    
 }
---
+++ src/main/webapp/WEB-INF/templates/jsp/admin/edit.jsp
@@ -0,0 +1,17 @@
+<%-- 
+    Document   : edit
+    Created on : Sep 3, 2013, 8:32:26 PM
+    Author     : sidratul
+--%>
+
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>JSP Page</title>
+    </head>
+    <body>
+        <h1>Hello World!</h1>
+    </body>
+</html>
