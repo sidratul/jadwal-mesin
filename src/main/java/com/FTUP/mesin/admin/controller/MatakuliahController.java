554b0977cd5571c11f068f5b9483f5f708672369
Muhammad Sidratul Muntaha <Muhammad.mochi@gmail.com>
2013-09-19 15:03:13 +0700

    lupa listener org.springframework.web.context.ContextLoaderListener

--- src/main/java/com/FTUP/mesin/admin/controller/MatakuliahController.java
+++ src/main/java/com/FTUP/mesin/admin/controller/MatakuliahController.java
@@ -1,13 +1,21 @@
 package com.FTUP.mesin.admin.controller;
 
+import com.FTUP.mesin.admin.dao.DosenDao;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class MatakuliahController {    
+    @Autowired private DosenDao dosenDao;
     
     @RequestMapping("/edit")
     public void editMatkul(){
         System.out.println("masuk");
-    }    
+    }
+    
+    @RequestMapping("/dosen")
+    public void tampilDosen(){
+        System.out.println(dosenDao.coba());
+    }
 }
--- src/main/java/com/FTUP/mesin/admin/dao/impl/MatakuliahDaoImp.java
+++ src/main/java/com/FTUP/mesin/admin/dao/impl/MatakuliahDaoImp.java
@@ -4,9 +4,9 @@ import com.FTUP.mesin.admin.dao.MataKuliahDao;
 import com.FTUP.mesin.admin.model.MataKuliah;
 import java.util.List;
 
-public class MatakuliahDaoSpringJdbc implements MataKuliahDao{
+public class MatakuliahDaoImp implements MataKuliahDao{
     
-    //private static final String SQL_GETALL_MATKUL = ""
+    private static final String SQL_GETALL_MATKUL = "SELECT * FROM MATAKULIAH";
     
     public void saveMatKul(MataKuliah mk) {        
     }
