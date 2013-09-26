6bbe02db36f3670264ad0e08e3c7989f49978fbd
Muhammad Sidratul Muntaha <Muhammad.mochi@gmail.com>
2013-09-26 17:29:39 +0700

    pengumunan selesai

---
+++ src/main/java/com/FTUP/mesin/admin/controller/PengumumanController.java
@@ -0,0 +1,49 @@
+package com.FTUP.mesin.admin.controller;
+
+import com.FTUP.mesin.admin.dao.PengumumanDao;
+import com.FTUP.mesin.admin.model.Pengumuman;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+@RequestMapping("/pengumuman")
+public class PengumumanController {
+    @Autowired PengumumanDao pengumumanDao;
+    
+    @RequestMapping("/tampil")
+    public void tampilPengumuman(ModelMap modelMap){
+        List<Pengumuman> pengumumans = pengumumanDao.getAllPengumuman();
+        modelMap.addAttribute("listPengumuman", pengumumans);
+    }
+    
+    @RequestMapping(value = "/input" , method = RequestMethod.GET)
+    public void tampilFormPengumuman(@RequestParam(value = "id",required = true ) Integer id,
+    ModelMap modelMap){
+        Pengumuman pengumuman = pengumumanDao.getPengumumanById(id);
+        if(pengumuman == null){
+            pengumuman = new Pengumuman();
+        }
+        
+        modelMap.addAttribute("pengumuman", pengumuman);
+    }
+    
+    @RequestMapping(value = "/input" , method = RequestMethod.GET)
+    public String prosesFormPengumuman(@ModelAttribute Pengumuman pengumuman,
+    ModelMap modelMap){
+        pengumumanDao.savePengumuman(pengumuman);
+        return "redirect:tampil";
+    }
+    
+    @RequestMapping("/hapus")
+    public String tampilPengumuman(@RequestParam("id") Integer id,
+    ModelMap modelMap){
+        pengumumanDao.deletePengumuman(id);
+        return "redirect:tampil";
+    }
+}
--- src/main/java/com/FTUP/mesin/admin/dao/impl/PengumumanDaoImpl.java
+++ src/main/java/com/FTUP/mesin/admin/dao/impl/PengumumanDaoImpl.java
@@ -2,26 +2,61 @@ package com.FTUP.mesin.admin.dao.impl;
 
 import com.FTUP.mesin.admin.dao.PengumumanDao;
 import com.FTUP.mesin.admin.model.Pengumuman;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.List;
+import javax.sql.DataSource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.simple.ParameterizedRowMapper;
+import org.springframework.stereotype.Repository;
 
+@Repository("pengumumanDao")
 public class PengumumanDaoImpl implements PengumumanDao{
+
+    private static final String SQL_GETALL_PENGUMUMAN = "SELECT * FROM PENGUMUMAN";
+    private static final String SQL_PENGUMUMAN_BYID = "SELECT * FROM PENGUMUMAN WHERE ID=?";
+    private static final String SQL_DELETE_PENGUMUMAN = "DELETE FROM PENGUMUMAN";
+    private static final String SQL_INSERT_PENGUMUMAN = "INSERT INTO `PENGUMUMAN` (`Text_Pengumuman`)VALUES(?)";
+    private static final String SQL_UPDATE_PENGUMUMAN = "UPDATE `PENGUMUMAN` SET `Text_Pengumuman`= ? WHERE ID = ?";
     
+    private JdbcTemplate jdbcTemplate;
     
+    private static final class PengumumanParameterizedRowMapper implements ParameterizedRowMapper<Pengumuman>{
+
+        public Pengumuman mapRow(ResultSet rs, int i) throws SQLException {
+            Pengumuman pengumuman = new Pengumuman();
+            pengumuman.setId(rs.getInt("ID"));
+            pengumuman.setTextPengumuman(rs.getString("Text_Pengumunan"));
+            return pengumuman;
+        }
+    }
+    
+    @Autowired
+    public void setDataSource(DataSource dataSource){
+        this.jdbcTemplate = new JdbcTemplate(dataSource);
+    }
     
     public List<Pengumuman> getAllPengumuman() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        List<Pengumuman> pengumumans = jdbcTemplate.query(SQL_GETALL_PENGUMUMAN,new PengumumanParameterizedRowMapper());
+        return pengumumans;
     }
 
     public void savePengumuman(Pengumuman pengumuman) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        if(pengumuman.getId() != null){
+            jdbcTemplate.update(SQL_UPDATE_PENGUMUMAN, new Object[]{pengumuman.getTextPengumuman(), pengumuman.getId()});
+        }else{
+            jdbcTemplate.update(SQL_INSERT_PENGUMUMAN, new Object[]{pengumuman.getTextPengumuman()});
+        }
     }
 
     public Pengumuman getPengumumanById(Integer id) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        Pengumuman pengumuman = jdbcTemplate.queryForObject(SQL_PENGUMUMAN_BYID, new PengumumanParameterizedRowMapper(),id);
+        return pengumuman;
     }
 
     public void deletePengumuman(Integer id) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        jdbcTemplate.update(SQL_DELETE_PENGUMUMAN);
     }
-    
 }
+
