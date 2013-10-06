2f9176581c167e52dda9d81058664e5c32ea985e
Muhammad Sidratul Muntaha <Muhammad.mochi@gmail.com>
2013-10-06 22:46:13 +0700

    ubah direktori file ireport + buat DocumentController

---
+++ src/main/java/com/FTUP/mesin/dokumen/controller/DosenDocumentController.java
@@ -0,0 +1,5 @@
+package com.FTUP.mesin.dokumen.controller;
+
+public class DosenDocumentController {
+    
+}
---
+++ src/main/java/com/FTUP/mesin/dokumen/controller/MatakuliahDocumentController.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.FTUP.mesin.dokumen.controller;
+
+/**
+ *
+ * @author sidratul
+ */
+public class MatakuliahDocumentController {
+    
+}
